7011e0a891c429243e24be1e9b0b95007b6e5460
Justein <54581873+justinebaldz@users.noreply.github.com>
2022-07-09 00:03:19 +0800

    commit on main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { DownloadComponent } from './pages/download/download.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AboutComponent, ContactComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    FaqsComponent,
+    DownloadComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatExpansionModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
